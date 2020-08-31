7450c9981a4ecfa9f7b8e18d37f5ee08f68d4383
Nantipam <keymasterviriya1150@gmail.con>
2020-08-31 20:31:37 +0700

    ⚗  Experiment new things$ 1

--- hoc/generateMain.js
+++ hoc/generateMain.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Lib from "../components/Lib";
-const generateMain = () => {
-  return (
-    <React.Fragment>
-      <Lib />
-    </React.Fragment>
-  );
+import SideBar from "../components/SideBar";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import DashBoard from "../components/DashBoard";
+import Logout from "../components/modal/Logout";
+const generateMain = (Component) => {
+  const WrappenMain = (props) => {
+    return (
+      <React.Fragment>
+        <Lib />
+        {/* Page Wrapper */}
+        <div id="wrapper">
+          <SideBar />
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <Navbar />
+             <Component {...props}/>
+            </div>
+            <Footer />
+          </div>
+        </div>
+        <a className="scroll-to-top rounded" href="#page-top">
+          <i className="fas fa-angle-up" />
+        </a>
+        {/* Logout Modal*/}
+        <Logout />
+      </React.Fragment>
+    );
+  };
+  return WrappenMain
 };
 
 export default generateMain;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,8 @@
-import Head from "next/head";
-import Lib from "../components/Lib";
-import SideBar from "../components/SideBar";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import DashBoard from "../components/DashBoard";
-import Logout from '../components/modal/Logout'
+import generateMain from '../hoc/generateMain'
+const DashBoardWithLayOut = generateMain(DashBoard) 
 export default function Home() {
   return (
-    <div>
-      <Lib />
-      {/* Page Wrapper */}
-      <div id="wrapper">
-        <SideBar />
-        <div id="content-wrapper" className="d-flex flex-column">
-          <div id="content">
-            <Navbar />
-            <DashBoard /> 
-          </div>
-      
-          <Footer />
-        </div>
-     
-      </div>
-      <a className="scroll-to-top rounded" href="#page-top">
-        <i className="fas fa-angle-up" />
-      </a>
-      {/* Logout Modal*/}
-      <Logout />
-    </div>
+    <DashBoardWithLayOut />
   );
 }
