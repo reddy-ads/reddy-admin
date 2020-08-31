03c35b58ce00938f4a5b74fdd274496d87a73bd8
Nantipam <keymasterviriya1150@gmail.con>
2020-08-31 20:19:12 +0700

    ⚗  Experiment new things$ 1

---
+++ hoc/generateMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Lib from "../components/Lib";
+const generateMain = () => {
+  return (
+    <React.Fragment>
+      <Lib />
+    </React.Fragment>
+  );
+};
+
+export default generateMain;
