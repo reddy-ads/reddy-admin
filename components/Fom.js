1bfaa57ef4aeed0b0ec172115572247c8cfc7a3d
Nantipam <keymasterviriya1150@gmail.con>
2020-08-31 21:23:28 +0700

    🚧  work in progress

---
+++ components/Fom.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Form  = () => {
+    return ( <div>test</div> );
+}
+ 
+export default Form ;
---
+++ pages/editor.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EditorJS from '@editorjs/editorjs';
+const editor = new EditorJS();
+const Editor = () => {
+    return ( <div>test</div> );
+}
+ 
+export default Editor;
