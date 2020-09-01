c5d2116fb572d40def1b36197a35480670012ad1
NantipatSoften <keymasterviriya1150@gmail.com>
2020-09-01 11:23:59 +0700

    🚚  move file

--- components/Fom.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Form  = () => {
-    return ( <div>test</div> );
-}
- 
-export default Form ;
--- pages/form.js
+++ pages/form.js
@@ -1,30 +1,9 @@
 import React from "react";
-import { Editor } from "@tinymce/tinymce-react";
-
+import generateMain from "../hoc/generateMain";
+import EditorTinymce from "../components/EditorTinymce";
+const EditorTinymceWithLayOut = generateMain(EditorTinymce);
 const Form = () => {
-  const handleEditorChange = (content, editor) => {
-    console.log("Content was updated:", content);
-  };
-  return (
-    <Editor
-      initialValue="<p>This is the initial content of the editor</p>"
-      init={{
-        height: 500,
-        menubar: `insert`,
-        plugins: [
-          "advlist autolink lists link image charmap print preview anchor",
-          "searchreplace visualblocks code fullscreen",
-          "insertdatetime media table paste code help wordcount",
-          "image",
-        ],
-        toolbar:
-          "undo redo | formatselect | bold italic backcolor | \
-          alignleft aligncenter alignright alignjustify | \
-          bullist numlist outdent indent | removeformat | help | image",
-      }}
-      onEditorChange={handleEditorChange}
-    />
-  );
+  return <EditorTinymceWithLayOut />;
 };
 
 export default Form;
